934a77bff1fd50b3a5900a54a3d1dadb797c55b0
ayush-001010 <ayushchakraborty001010@gmail.com>
2026-09-08 20:09:10 +0530

    common API

--- src/Controller/Common.ts
+++ src/Controller/Common.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import CommonService from "../Service/Common/CommonService";
+import CloudFactory from "../Service/Cloud/CloudFactory";
 
 export const activites = async (req: Request, res: Response) => {
     try {
@@ -18,6 +19,25 @@ export const activites = async (req: Request, res: Response) => {
             return res.status(404).json({ error: "Activity not found" });
         }
     } catch (error) {
+        console.log("Error  ", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const someRandomImages = async (req: Request, res: Response) => {
+    try {
+        const {type} = req.query;
+        switch(type) {
+            case "emptyChatBox":{
+                const cloudServiceInstance = CloudFactory.getCloudServiceInstance();
+                return res.send({
+                    success : true,
+                    data : cloudServiceInstance.getImages("Common/—Pngtree—cool travelling van tropical_8164641.png").data  
+                })
+            }
+        }
+    }
+    catch(error) {
         console.log("Error  ", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
